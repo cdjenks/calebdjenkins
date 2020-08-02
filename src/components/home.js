11d9ad6af4ca1607aa3da1085633063fa94b08db
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-08-02 09:25:00 -0400

    Still adjusting styling, spacing etc. Changed background images as well

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import "../styles/index.css";
 
 function Home() {
     return (
-        <div className="container">
+        <div className="container about-container">
 
             <div className="row content-main">
 
