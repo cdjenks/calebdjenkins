718ae6a9ac5e9c106ad21c57626753d62caa935b
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-08-04 17:30:15 -0400

    Continuing to tweak styling. Main change was to the bio text - much improved

--- src/components/home.js
+++ src/components/home.js
@@ -19,12 +19,14 @@ function Home() {
 
                         <hr />
 
-                        <p className="bio-text">I am a very inexperienced yet motivated web developer hoping to break into web development professionally. To this
-                        point my work experience and education has been entirely focused in the hospitality and tourism fields, but I am excited
-                        to be making a pivot into a new, more rewarding career. I look forward to translating my problem solving abilities into
-                        the world of computer programming, and I thrive in collaborative settings. At the end of a day I want to have something I can
-                        point to and say that I helped bring it into existence. Even when computer programming is not flashy, which seems to be usually,
-                        I love the satisfaction that I get from coding.
+                        <p className="bio-text">I am a newly minted but motivated web developer looking to break into web development professionally. To this
+                        point my work experience and education has been focused in the hospitality and tourism field, but I am excited
+                        to be making a dive into a new, more rewarding career. I earned a Bachelor's degree in Parks, Recreation, and Tourism Management
+                        from NC State University in 2017, but I have now taken my education in an entirely different direction since enrolling in UNC's Coding Bootcamp.
+                        The course proved to be every bit as difficult as advertised but also invaluable as a jumping off point into the world of web development.
+                        I now look forward to translating my problem solving abilities into the profession of computer programming and continuing to develop my skillset
+                        in this arena. At the end of a day I want to have something to point to and say "I helped bring that into existence," and even when computer
+                        programming is not flashy, which seems to be usually, I love the satisfaction that I get from coding.
                         </p>
 
                     </section>
--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -11,7 +11,7 @@ class ProjectCard extends Component {
             <div className="card project-card">
                 <h2 className="card-header">{name}</h2>
                 <a href={link} target="_blank"><img className="card-img card-body app-screenshot"
-                    src={thumbnail} alt="App Image"></img></a>
+                    src={`${process.env.PUBLIC_URL}${thumbnail}`} alt="App Image"></img></a>
                 <p>click image to view app</p>
             </div>
         )
